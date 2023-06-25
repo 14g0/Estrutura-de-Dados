951eb8930757f4286ce0305ccf1a848e1839968a
Iago <iagocesarts@hotmail.com>
2023-06-25 18:15:01 -0300

    Adição do programa feito pelo Gepeto.

---
+++ cidr.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct no {
+    int chave;
+    char *valor;
+    struct no *dir, *esq;
+} no;
+
+/*
+typedef struct numBin {
+    char 
+} numBin;
+*/
+
+/*----------------------------------------------------------------------------*/
+
+/* Recebe os inputs das CIDRs e os aloca    */
+void lerInput(void);
+
+/*----------------------------------------------------------------------------*/
+
+int main(void) {
+
+}
+
+/*----------------------------------------------------------------------------*/
+
+void lerInput(void) {
+    
+}
